085c9ad213fe35af136bca5b765de7bebcd08b5d
nepalk-scram <kirtan.nepal@scramsystems.com>
2026-01-06 14:06:24 +0545

    feat: simplify login by auto-completing default domain

--- AttendenceSystem/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AttendenceSystem/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -61,26 +61,14 @@ namespace AttendenceSystem.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            [Display(Name = "Username or Email")]
+            public string UserNameOrEmail { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Display(Name = "Remember me?")]
             public bool RememberMe { get; set; }
         }
@@ -110,9 +98,15 @@ namespace AttendenceSystem.Areas.Identity.Pages.Account
 
             if (ModelState.IsValid)
             {
+                var userName = Input.UserNameOrEmail;
+                if (!userName.Contains("@"))
+                {
+                    userName += "@company.com";
+                }
+
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(userName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
