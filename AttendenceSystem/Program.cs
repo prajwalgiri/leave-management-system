cfcc4f41cb780dad79e12fbe957fef9e296cff16
nepalk-scram <kirtan.nepal@scramsystems.com>
2026-01-06 13:58:51 +0545

    feat: enable Razor Runtime Compilation for immediate UI updates

--- AttendenceSystem/Program.cs
+++ AttendenceSystem/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
